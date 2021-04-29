7203afa4254011ae704653e090bfe06144c2a709
Vignesh <vigneshpillutla@gmail.com>
2021-04-29 13:30:43 +0530

    cors

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const sessionStore = MongoStore.create({
     mongoUrl:db_string,
     collectionName:'sessions'
 });
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors({
     origin:['http://localhost:3000','https://keep-er.netlify.app'],
     credentials:true
