77d988664b5f45b3b6fbc43324332abdf6a05c23
Vignesh <vigneshpillutla@gmail.com>
2021-04-29 13:16:14 +0530

    modified cors

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', 'https://keep-er.netlify.app');
+  next();
+})
 app.use(session({
     secret:process.env.SECRET,
     resave:false,
