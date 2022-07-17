e925e08516ca770633002fd4c17b73d6e5126a35
Vignesh <vigneshpillutla@gmail.com>
2022-07-18 03:30:21 +0530

    Added trust proxy

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,7 @@ let corsOptions: CorsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set("trust proxy",1);
 
 export default {
   app,
