38aeebd80c4ff42868679eed4fe6c392977ac476
Vignesh Pillutla <vignesh.pillutla@zeuslearning.com>
2025-04-13 20:41:47 +0530

    Add route to fetch user details.

--- src/main/java/com/keeper/api/controller/UserController.java
+++ src/main/java/com/keeper/api/controller/UserController.java
@@ -6,10 +6,13 @@ import com.keeper.api.entity.User;
 import com.keeper.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/api/user")
@@ -17,4 +20,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     private final UserService userService;
 
+    @GetMapping
+    public ResponseEntity<ApiResponse<?>> getUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = (String) authentication.getPrincipal();
+        User user = userService.getUserByUsername(username);
+
+        return ResponseEntity.ok(ApiResponse.success(Map.of(
+                "id", user.getId(),
+                "username", user.getUsername(),
+                "createdAt", user.getCreatedAt(),
+                "notes", user.getNotes()
+        )));
+    }
 }
